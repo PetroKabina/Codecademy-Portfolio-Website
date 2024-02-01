af3e261c0204a985d10753fdb0eb0a8b6d0a8a88
abc <abc@example.com>
2024-02-01 17:02:49 +0200

    randomizer

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
+const randomizer = (min, max) => {
+    if (min >= 0) {
+        return Math.ceil(Math.random() * (max - min) + min);
+    } else {
+        return Math.ceil(Math.random() * min + max/2);
+    }
+}
+
 let menuItems = document.getElementsByClassName("menu_item");
 let mainMenuButtons = document.querySelectorAll("header nav #head_menu .menu_item");
 
@@ -15,23 +23,24 @@ for (let item of menuItems) {
 }
 
 for (let item of mainMenuButtons) {
-    let zIndex = Math.ceil(Math.random() * mainMenuButtons.length);
-    let radius = {lt: Math.ceil(Math.random() * (80-40) + 40),
-                  rt: Math.ceil(Math.random() * (80-40) + 40),
-                  lb: Math.ceil(Math.random() * (80-40) + 40),
-                  rb: Math.ceil(Math.random() * (80-40) + 40),};
-    let rotation = Math.ceil(Math.random() * 30 - 15);
-    let margin = Math.ceil(Math.random() * (-10));
-    let padding = Math.ceil(Math.random() * (15 - 10) + 10);
-    let rRand = Math.ceil(Math.random() * (150 - 20) + 20);
-    let gRand = Math.ceil(Math.random() * (150 - 70) + 70);
-    let bRand = Math.ceil(Math.random() * (150 - 20) + 20);
+    let zIndex = randomizer(0, mainMenuButtons.length);
+    let radius = {lt: randomizer(80,40),
+                  rt: randomizer(80,40),
+                  lb: randomizer(80,40),
+                  rb: randomizer(80,40),};
+    let rotation = randomizer(-30,30);
+    let margin = randomizer(-10, 0);
+    let padding = 5 + randomizer(-15, 40);
+    let rRand = randomizer(20, 150);
+    let gRand = randomizer(70, 150);
+    let bRand = randomizer(70, 150);
     let randomBGColor = "rgb(" + rRand + "," + gRand + ","+ bRand + ")";
     item.style.backgroundColor = randomBGColor;
     item.style.margin = margin + "px";
     item.style.padding = padding + "px";
     item.style.rotate = rotation + "deg";
     item.style.zIndex = zIndex;
+    item.style.border = "solid 3px " + "rgb(" + (rRand - 50) + "," + (gRand - 50) + ","+ (bRand - 50) + ")";;
     item.style.borderRadius = radius.lt + "% " + radius.rt + "% " + radius.lb + "% " + radius.rb + "%";
     console.log(item, radius.lb + " " + radius.lt);
 }
