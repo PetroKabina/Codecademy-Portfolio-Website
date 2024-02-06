4b357e7decfc750cbcd44d4c56f8c550cee1f9b5
Kostiantyn <kostiantyn.pavliuk@globallogic.com>
2024-02-06 22:37:31 +0200

    responsive design; minor layout changes

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const rand = (min, max) => {
     if (min >= 0) {
         return Math.ceil(Math.random() * (max - min) + min);
     } else {
-        return Math.ceil(Math.random() * min + max/2);
+        return Math.ceil(Math.random() * min + max / 2);
     }
 }
 
@@ -11,7 +11,7 @@ const randColorPicker = () => {
     let rRand = rand(20, 80);
     let gRand = rand(20, 80);
     let bRand = rand(20, 80);
-    return "rgb(" + rRand + "," + gRand + ","+ bRand + ")";
+    return "rgb(" + rRand + "," + gRand + "," + bRand + ")";
 }
 
 const clockTick = () => {
@@ -26,13 +26,13 @@ const clockTick = () => {
         offset = currentTime.getTimezoneOffset(),
         dayName = undefined;
     const addNull = num => {
-        return num < 10 ? '0'+ num : num;
+        return num < 10 ? '0' + num : num;
     }
 
-hours = addNull(hours);
-minutes = addNull(minutes);
-seconds = addNull(seconds);
-    switch(dow) {
+    hours = addNull(hours);
+    minutes = addNull(minutes);
+    seconds = addNull(seconds);
+    switch (dow) {
         case 1:
             dayName = 'Monday';
             break;
@@ -56,82 +56,89 @@ seconds = addNull(seconds);
             break;
         default: undefined;
     }
-    
-let offsetFormat = ' (GMT' + ( offset < 0 ? '+' + -(offset/60) : (offset/60) ) + ')';
 
-let text = (dayName + '<br>' + month + "/" + day + "/" + year + ' ' + '<br>' + hours + ':' + minutes + ':' + seconds + ' ' + offsetFormat);
-// here we get the element with the id of "date" and change the content to the text variable we made above
-document.getElementById('tod').innerHTML = text;
+    let offsetFormat = ' (GMT' + (offset < 0 ? '+' + -(offset / 60) : (offset / 60)) + ')';
+
+    let text = (dayName + ', ' + month + "/" + day + "/" + year + ' ' + '' + hours + ':' + minutes + ':' + seconds + ' ' + offsetFormat);
+    // here we get the element with the id of "date" and change the content to the text variable we made above
+    document.getElementById('tod').innerHTML = text;
 }
 
 const applyEffectsOnClass = (className) => {
-var elements = document.getElementsByClassName(className);
+    var elements = document.getElementsByClassName(className);
     for (let item of elements) {
         item.onmouseenter = mouseEnter;
         item.onmouseleave = mouseLeave;
     }
 }
 // here we run the clockTick function every 100ms
-setInterval(clockTick, 500);
+setInterval(clockTick, 100);
 
 const tod = document.getElementById("tod"),
-mainMenuButtons = document.querySelectorAll("header nav .menu_item"),
-footerMenuButtons = document.querySelectorAll("footer nav .menu_item"),
-firstContentPLetter = document.querySelector("#content p:first-of-type::first-letter"),
-link = document.querySelector('main #content a'),
-cards = document.getElementsByClassName("card"),
-cardDescriptions = document.getElementsByClassName("card_desc"),
-expandButton = document.getElementsByClassName("expand_button");
+    mainMenuButtons = document.querySelectorAll("header nav .menu_item"),
+    footerMenuButtons = document.querySelectorAll("footer nav .menu_item"),
+    firstContentPLetter = document.querySelector("#content p:first-of-type::first-letter"),
+    links = document.querySelectorAll('main a'),
+    cards = document.getElementsByClassName("card"),
+    cardDescriptions = document.getElementsByClassName("card_desc"),
+    expandButton = document.getElementsByClassName("expand_button");
 
 //firstContentPLetter.style.color = "rgb(" + rand(50, 100) + "," + rand(50, 100) + "," + rand(50, 100) + ")";
 
 const mouseEnter = (event) => {
     event.target.style.textShadow = "1px 1px 1px black";
     event.target.style.color = "lightgray";
-    event.target.style.filter= "saturate(0)";
+    event.target.style.filter = "saturate(0)";
 }
 
 const mouseLeave = (event) => {
     event.target.style.color = randColorPicker();
     event.target.style.textShadow = "";
-    event.target.style.filter= "";
+    event.target.style.filter = "";
 }
 
 //if (window.matchMedia("(min-width: 1000px)").matches) {
-    // mouse hover effects
-    //link.onmouseenter = mouseEnter;
-    //link.onmouseleave = mouseLeave;
+// mouse hover effects
+for (let item of links) {
+    item.style.color = randColorPicker();
+    item.style.textDecoration = 'underline';
+    item.onmouseenter = mouseEnter;
+    item.onmouseleave = mouseLeave; 
+}
 
-    applyEffectsOnClass('menu_item');
+applyEffectsOnClass('menu_item');
 
-    // MAIN MENU EFFECTS
-    for (let item of mainMenuButtons) {
-        // RANDOM VALUES DECLARATION
-        let top = rand(0, 20),
+// MAIN MENU EFFECTS
+for (let item of mainMenuButtons) {
+    // RANDOM VALUES DECLARATION
+    let top = rand(0, 20),
         zIndex = rand(0, mainMenuButtons.length),
-        radius =   {lt: rand(80,40),
-                    rt: rand(80,40),
-                    lb: rand(80,40),
-                    rb: rand(80,40),},
+        radius = {
+            lt: rand(80, 40),
+            rt: rand(80, 40),
+            lb: rand(80, 40),
+            rb: rand(80, 40),
+        },
         margin = rand(-5, 0),
         padding = 5 + rand(-15, 40);
-        // RANDOMIZING THE LAYOUT
-        item.style.backgroundColor = randColorPicker();
-        item.style.margin = margin + "px";
-        item.style.padding = padding + "px";
-        item.style.zIndex = zIndex;
-        item.style.border = "solid 3px " + randColorPicker();
-        item.style.borderRadius = radius.lt + "% " + radius.rt + "% " + radius.lb + "% " + radius.rb + "%";
-        item.style.top = top + 'px';
-    }
+    // RANDOMIZING THE LAYOUT
+    item.style.color = randColorPicker();
+    item.style.backgroundColor = randColorPicker();
+    item.style.margin = margin + "px";
+    item.style.padding = padding + "px";
+    item.style.zIndex = zIndex;
+    item.style.border = "solid 3px " + randColorPicker();
+    item.style.borderRadius = radius.lt + "% " + radius.rt + "% " + radius.lb + "% " + radius.rb + "%";
+    item.style.top = top + 'px';
+}
 
-    for (let item of footerMenuButtons) {
-        let rRand = rand(20, 80);
-        let gRand = rand(20, 80);
-        let bRand = rand(20, 80);
-        let randomBGColor = "rgb(" + rRand + "," + gRand + ","+ bRand + ")";
-        item.style.color = randomBGColor;
-    }
+for (let item of footerMenuButtons) {
+    let rRand = rand(20, 80);
+    let gRand = rand(20, 80);
+    let bRand = rand(20, 80);
+    let randomBGColor = "rgb(" + rRand + "," + gRand + "," + bRand + ")";
+    item.style.color = randomBGColor;
+}
 
 //} else {
 /*    for (let item of mainMenuButtons) {
