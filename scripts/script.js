4ab887c95668621c1fe890d7a8209243310fdab4
Kostiantyn <kostiantyn.pavliuk@globallogic.com>
2024-02-08 04:13:59 +0200

    clicking external links opens a new tab; adaptive design; layout changes

--- scripts/script.js
+++ scripts/script.js
@@ -7,10 +7,10 @@ const rand = (min, max) => {
     }
 }
 
-const randColorPicker = () => {
-    let rRand = rand(20, 80);
-    let gRand = rand(20, 80);
-    let bRand = rand(20, 80);
+const randColorPicker = (rMin=20, rMax=80, gMin=20, gMax=80, bMin=20, bMax=80) => {
+    let rRand = rand(rMin, rMax);
+    let gRand = rand(gMin, gMax);
+    let bRand = rand(bMin, bMax);
     return "rgb(" + rRand + "," + gRand + "," + bRand + ")";
 }
 
@@ -58,10 +58,14 @@ const clockTick = () => {
     }
 
     let offsetFormat = ' (GMT' + (offset < 0 ? '+' + -(offset / 60) : (offset / 60)) + ')';
-
-    let text = (dayName + ', ' + month + "/" + day + "/" + year + ' ' + '' + hours + ':' + minutes + ':' + seconds + ' ' + offsetFormat);
+    const tod = document.getElementById('tod');
     // here we get the element with the id of "date" and change the content to the text variable we made above
-    document.getElementById('tod').innerHTML = text;
+    if (window.matchMedia("only screen and (max-width: 1000px)").matches) {
+        var todText = (dayName + ', <br>' + month + "/" + day + "/" + year + '<br>' + '' + hours + ':' + minutes + ':' + seconds + ' ' + offsetFormat);
+    } else {
+        var todText = (dayName + ', ' + month + "/" + day + "/" + year + ' ' + '' + hours + ':' + minutes + ':' + seconds + ' ' + offsetFormat);
+    }
+    tod.innerHTML = todText;
 }
 
 const applyEffectsOnClass = (className) => {
@@ -74,6 +78,7 @@ const applyEffectsOnClass = (className) => {
 // here we run the clockTick function every 100ms
 setInterval(clockTick, 100);
 
+// INTERACTIVE ELEMENTS SELECTORS
 const tod = document.getElementById("tod"),
     mainMenuButtons = document.querySelectorAll("header nav .menu_item"),
     footerMenuButtons = document.querySelectorAll("footer nav .menu_item"),
@@ -81,35 +86,64 @@ const tod = document.getElementById("tod"),
     links = document.querySelectorAll('main a'),
     cards = document.getElementsByClassName("card"),
     cardDescriptions = document.getElementsByClassName("card_desc"),
-    expandButton = document.getElementsByClassName("expand_button");
+    expandButton = document.getElementsByClassName("expand_button"),
+    menus = document.querySelectorAll('nav, menu');
 
 //firstContentPLetter.style.color = "rgb(" + rand(50, 100) + "," + rand(50, 100) + "," + rand(50, 100) + ")";
 
-const mouseEnter = (event) => {
+/*const expandCard = event => {
+    event.target.style.minHeigth = 'fit-content';
+    event.target.style.overflow = 'visible';
+}*/
+
+const mouseEnter = event => {
     event.target.style.textShadow = "1px 1px 1px black";
     event.target.style.color = "lightgray";
     event.target.style.filter = "saturate(0)";
 }
 
-const mouseLeave = (event) => {
+const mouseLeave = event => {
     event.target.style.color = randColorPicker();
     event.target.style.textShadow = "";
     event.target.style.filter = "";
 }
 
+//expand card
+/*for (let item of cards) {
+    item.onmouseclick = expandCard;
+}*/
+
 //if (window.matchMedia("(min-width: 1000px)").matches) {
 // mouse hover effects
 for (let item of links) {
-    item.style.color = randColorPicker();
-    item.style.textDecoration = 'underline';
-    item.onmouseenter = mouseEnter;
-    item.onmouseleave = mouseLeave; 
+    /*if (window.matchMedia("only screen and (max-width: 1000px)").matches) {
+        item.style.color = randColorPicker();
+        item.style.textDecoration = 'underline';
+        item.onmouseenter = mouseEnter;
+        item.onmouseleave = mouseLeave; 
+    } else {*/
+        item.style.color = randColorPicker();
+        item.style.textDecoration = 'underline';
+        item.onmouseenter = mouseEnter;
+        item.onmouseleave = mouseLeave; 
+    //}
 }
 
 applyEffectsOnClass('menu_item');
 
 // MAIN MENU EFFECTS
 for (let item of mainMenuButtons) {
+    /*if (window.matchMedia("only screen and (max-width: 1000px)").matches) {
+    //DISABLE RANDOMITY
+        item.style.color = 'green';
+        item.style.backgroundColor = 'black';
+        item.style.margin = "0px";
+        item.style.padding = "0px";
+        item.style.zIndex = 0;
+        item.style.border = "none";
+        item.style.borderRadius = '0px';
+        item.style.top = '0px';
+    } else {*/
     // RANDOM VALUES DECLARATION
     let top = rand(0, 20),
         zIndex = rand(0, mainMenuButtons.length),
@@ -122,7 +156,8 @@ for (let item of mainMenuButtons) {
         margin = rand(-5, 0),
         padding = 5 + rand(-15, 40);
     // RANDOMIZING THE LAYOUT
-    item.style.color = randColorPicker();
+    item.style.textShadow = 'none';
+    item.style.color = randColorPicker(100,180,120,180,80,150,120,180);
     item.style.backgroundColor = randColorPicker();
     item.style.margin = margin + "px";
     item.style.padding = padding + "px";
@@ -130,6 +165,15 @@ for (let item of mainMenuButtons) {
     item.style.border = "solid 3px " + randColorPicker();
     item.style.borderRadius = radius.lt + "% " + radius.rt + "% " + radius.lb + "% " + radius.rb + "%";
     item.style.top = top + 'px';
+    //}
+}
+
+for (let item of menus) {
+    const top = rand(1,5);
+    const left = rand(1,5);
+    const spread = rand(10, 20);
+    const color = randColorPicker(150,255,200,255,150,255,150,255);
+    item.style.textShadow = top + 'px ' + left + 'px ' + spread + 'px ' + color + ', ' + -top + 'px ' + left + 'px ' + spread + 'px ' + color + ', ' + top + 'px ' + -left + 'px ' + spread + 'px ' + color + ', ' + -top + 'px ' + -left + 'px ' + spread + 'px ' + color;
 }
 
 for (let item of footerMenuButtons) {
